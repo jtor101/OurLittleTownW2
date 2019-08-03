231be5c8a072b1590b06b549b0b0adf73f0722f9
John Torres <jtorres0802@gmail.com>
2019-08-03 12:24:32 -0400

    Updated spacing, comments, functionality.

--- scripts/oltscript.js
+++ scripts/oltscript.js
@@ -9,7 +9,7 @@
 
 function getNumAdults() { // This function acquires the number of adults staying at the hotel from the Number of Adults dropdown.
     let adultNumSelect = document.getElementById("adultNumSelect")
-    let numOfAdults = 0;
+    let numOfAdults = 1;
 
     switch (adultNumSelect.options[adultNumSelect.selectedIndex].value) {
         case "1":
@@ -56,8 +56,10 @@ function getNumChildren() { // This function acquires the number of children sta
 
 function getCheckoutDate() { //This function gets the checkout date based on the information in the Check-In Date date picker and Length of Stay field.  It then outputs the Check-In and Check-Out Dates to the Hotel Reservation Costs section.
     let checkinDateField = document.getElementById("checkinDateField");
+
     let s = checkinDateField.value;
     let checkinDate = new Date();
+
     checkinDate.setMonth(s.substring(5, 7) - 1);
     checkinDate.setDate(s.substring(8, 10));
     checkinDate.setFullYear(s.substring(0, 4));
@@ -65,11 +67,12 @@ function getCheckoutDate() { //This function gets the checkout date based on the
     let stayLengthField = document.getElementById("stayLengthField");
     let stayLength = Number(stayLengthField.value);
 
-    let msec_per_day = 1000 * 60 * 60 * 24
+    let msec_per_day = 1000 * 60 * 60 * 24;
 
     let checkoutMsec = checkinDate.getTime() + (stayLength * msec_per_day);
 
     document.getElementById("checkinDateOutput").value = checkinDate.toLocaleDateString();
+    
     let temp = new Date(checkoutMsec);
     document.getElementById("checkoutDateOutput").value = temp.toLocaleDateString();
 }
@@ -100,6 +103,7 @@ function getRoomType() { // This function gets the room type information from th
 function checkRoomCompatibility() { // This function checks the number of occupants chosed in the adult and children dropdowns and hides incompatible rooms.
     let numOfAdults = getNumAdults();
     let numOfChildren = getNumChildren();
+
     let rmSelect = document.getElementById("typeOfRmSelect");
 
     if (numOfAdults + numOfChildren > 6) { // All are hidden
@@ -107,22 +111,26 @@ function checkRoomCompatibility() { // This function checks the number of occupa
         rmSelect.options[2].style.display = "none";
         rmSelect.options[3].style.display = "none";
         rmSelect.options[4].style.display = "none";
-    } else if (numOfAdults + numOfChildren > 5) { // King, Queen, and King Suite are hidden
+    } 
+    else if (numOfAdults + numOfChildren > 5) { // King, Queen, and King Suite are hidden
         rmSelect.options[1].style.display = "none";
         rmSelect.options[2].style.display = "none";
         rmSelect.options[3].style.display = "none";
         rmSelect.options[4].style.display = "block";
-    } else if (numOfAdults + numOfChildren > 4) { //King and Queen are hidden
+    } 
+    else if (numOfAdults + numOfChildren > 4) { //King and Queen are hidden
         rmSelect.options[1].style.display = "block";
         rmSelect.options[2].style.display = "none";
         rmSelect.options[3].style.display = "none";
         rmSelect.options[4].style.display = "block";
-    } else if (numOfAdults + numOfChildren > 2) { //King is hidden
+    } 
+    else if (numOfAdults + numOfChildren > 2) { //King is hidden
         rmSelect.options[1].style.display = "block";
         rmSelect.options[2].style.display = "none";
         rmSelect.options[3].style.display = "block";
         rmSelect.options[4].style.display = "block";
-    } else { //All are shown.
+    } 
+    else { //All are shown.
         rmSelect.options[1].style.display = "block";
         rmSelect.options[2].style.display = "block";
         rmSelect.options[3].style.display = "block";
@@ -141,11 +149,14 @@ function getDiscount() { //This function returns the discount amount based on th
 
     if (discAaaRb.checked) {
         discAmt = 0.1; //10% off, 90% of original price
-    } else if (discSeniorRb.checked) {
+    } 
+    else if (discSeniorRb.checked) {
         discAmt = 0.1; //10% off, 90% of original price
-    } else if (discMilitaryRb.checked) {
+    } 
+    else if (discMilitaryRb.checked) {
         discAmt = 0.2; //20% off, 80% of original price
-    } else if (discNoneRb.checked) {
+    } 
+    else if (discNoneRb.checked) {
         discAmt = 0;
     }
     return discAmt;
@@ -155,17 +166,21 @@ function getDiscount() { //This function returns the discount amount based on th
 function getBreakfastCost() { // This function returns the cost of breakfast based on the number of occupants, the Breakfast switch position, and the Discount radio button selected.
     let stayLengthField = document.getElementById("stayLengthField");
     let stayLength = stayLengthField.value;
+
     let breakfastSwitch = document.getElementById("breakfastSwitch");
     let discSeniorRb = document.getElementById("discSeniorRb");
     let breakfastCost = 0;
+
     let numOfAdults = getNumAdults();
     let numOfChildren = getNumChildren();
 
     if (discSeniorRb.checked && breakfastSwitch.checked) {
         breakfastCost = 0;
-    } else if (breakfastSwitch.checked) {
+    } 
+    else if (breakfastSwitch.checked) {
         breakfastCost = ((numOfAdults * 6.96) + (numOfChildren * 3.95)) * stayLength;
-    } else {
+    } 
+    else {
         breakfastCost = 0;
     }
     return breakfastCost;
@@ -176,7 +191,9 @@ function getBaseRoomCost() { //This function gets the base room cost based on th
     let stayLengthField = document.getElementById("stayLengthField");
     let stayLength = stayLengthField.value;
     let roomCost = 0;
+    
     let costOfRmType = getRoomType();
+
     roomCost = stayLength * costOfRmType
     return roomCost;
 }
